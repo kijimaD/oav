285095ddf77a3dad9cf92298fce4c90795da46be
kijimaD <norimaking777@gmail.com>
2023-02-09 22:53:22 +0900

    test: add

--- oa/validator_test.go
+++ oa/validator_test.go
@@ -37,7 +37,7 @@ func TestValidate(t *testing.T) {
 	url, _ := url.Parse(basePath)
 
 	buffer := bytes.Buffer{}
-	cli := New(&buffer, strings.NewReader(schemafile), *url)
+	cli := New(&buffer, strings.NewReader(schemafileA), *url)
 	err := cli.Run("/pets")
 	if err != nil {
 		log.Fatalf("!! %+v", err)
@@ -58,7 +58,7 @@ func TestValidateRouteNotMatch(t *testing.T) {
 	url, _ := url.Parse(basePath)
 
 	buffer := bytes.Buffer{}
-	cli := New(&buffer, strings.NewReader(schemafile), *url)
+	cli := New(&buffer, strings.NewReader(schemafileA), *url)
 	err := cli.Run("/not_exists")
 	if err != nil {
 		log.Fatalf("!! %+v", err)
@@ -66,13 +66,33 @@ func TestValidateRouteNotMatch(t *testing.T) {
 
 	got := buffer.String()
 	assert.Contains(t, got, "/not_exists")
-	assert.Contains(t, got, "no matching operation was found")
+}
+
+func TestValidateRouteInvalidResponse(t *testing.T) {
+	ts := testServer(t)
+	defer ts.Close()
+
+	// need to fix server address
+	url, _ := url.Parse(basePath)
+
+	buffer := bytes.Buffer{}
+	cli := New(&buffer, strings.NewReader(schemafileB), *url)
+	err := cli.Run("/pets")
+	if err != nil {
+		log.Fatalf("!! %+v", err)
+	}
+
+	got := buffer.String()
+	assert.Contains(t, got, "/pets")
+	assert.Contains(t, got, "value must be a string")
+	assert.Contains(t, got, "description\": \"pet ID")
+	assert.Contains(t, got, "type\": \"string")
 }
 
 func TestDumpRoutes(t *testing.T) {
 	buffer := bytes.Buffer{}
 	url, _ := url.Parse(basePath)
-	cli := New(&buffer, strings.NewReader(schemafile), *url)
+	cli := New(&buffer, strings.NewReader(schemafileA), *url)
 	err := cli.dumpRoutes()
 	if err != nil {
 		t.Fatal(err)
@@ -84,7 +104,7 @@ func TestDumpRoutes(t *testing.T) {
 	assert.Contains(t, got, "list_pets")
 }
 
-const schemafile = `---
+const schemafileA = `---
 openapi: "3.1.0"
 
 info:
@@ -171,6 +191,105 @@ components:
           - id: 1
             name: dog
           - id: 2
+            name: cat
+  parameters:
+    Limit:
+      name: limit
+      in: query
+      description: data count
+      required: false
+      schema:
+        type: integer
+        format: int32
+`
+
+const schemafileB = `---
+openapi: "3.1.0"
+
+info:
+  description: |
+    ## develop
+    hello world
+      - list
+        - A
+        - B
+
+  version: 1.0.0
+  title: API Docs
+  contact:
+    name: kijimad
+    email: norimaking777@gmail.com
+
+servers:
+  - url: http://localhost:8089
+    description: go server
+  - url: http://localhost:6969
+    description: mock(Prism)
+
+tags:
+  - name: Pet
+    description: |
+      pet
+
+paths:
+  /pets:
+    get:
+      summary: list pets
+      description: list pets
+      operationId: list_pets
+      tags:
+        - Pet
+      parameters:
+        - $ref: "#/components/parameters/Limit"
+      responses:
+        '200':
+          description: success
+          content:
+            application/json:
+              schema:
+                required:
+                  - pets
+                properties:
+                  pets:
+                    $ref: "#/components/schemas/Pets"
+              examples:
+                case1:
+                  $ref: "#/components/examples/PetsResponse"
+        default:
+          description: Unexpected error
+          content:
+            application/json:
+              schema:
+                $ref: "#/components/schemas/Error"
+
+components:
+  schemas:
+    Pets:
+      type: array
+      description: list pets
+      items:
+        properties:
+          id:
+            type: string
+            description: pet ID
+    Error:
+      required:
+        - code
+        - message
+      properties:
+        code:
+          type: integer
+          format: int32
+        message:
+          type: string
+  examples:
+    PetsResponse:
+      description: pets
+      value:
+        pets:
+          - id: "1"
+            name: dog
+          - id: "2"
             name: cat
   parameters:
     Limit:
